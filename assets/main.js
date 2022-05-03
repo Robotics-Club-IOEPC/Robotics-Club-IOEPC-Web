33e2152df0a5d648677a7e6b2f38b101dbb3a32f
sudipnext <parajulisudip123@outlook.com>
2022-05-03 06:57:59 +0545

    files added

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+  $("fullpage2").load("./components/fullpage2.html");
   $("footer").load("./components/footer.html");
+  $("fullpage").load("components/fullpage.html");
+  $("fullpage1").load("components/fullpage1.html");
   $("navbar").load("./components/navbar.html");
 });

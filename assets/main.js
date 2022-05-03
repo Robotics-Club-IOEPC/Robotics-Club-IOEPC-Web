e4635b1360401cfc6acbe7dd91d657c292494252
Dhiraj KC <62998336+DKC195@users.noreply.github.com>
2022-05-03 15:59:10 +0545

    Rearranged Folders

--- assets/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $("fullpage2").load("./components/fullpage2.html");
-  $("footer").load("./components/footer.html");
-  $("fullpage").load("components/fullpage.html");
-  $("fullpage1").load("components/fullpage1.html");
-  $("navbar").load("./components/navbar.html");
-});
---
+++ per-page/index/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $("fullpage").load("./per-page/index/html/fullpage.html");
+  $("fullpage1").load("./per-page/index/html/fullpage1.html");
+  $("fullpage2").load("./per-page/index/html/fullpage2.html");
+});

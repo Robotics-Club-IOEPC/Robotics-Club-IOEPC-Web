5a7c3ad7c761d3f48b91fcf0fa9ff0271c20af44
Dhiraj KC <62998336+DKC195@users.noreply.github.com>
2022-05-18 00:55:50 +0545

    Refined

--- per-page/index/index.js
+++ per-page/index/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
   $("fullpage").load("./per-page/index/html/fullpage.html");
-  $("fullpage1").load("./per-page/index/html/fullpage1.html");
-  $("fullpage2").load("./per-page/index/html/fullpage2.html");
+  // $("fullpage1").load("./per-page/index/html/fullpage1.html");
+  // $("fullpage2").load("./per-page/index/html/fullpage2.html");
 });

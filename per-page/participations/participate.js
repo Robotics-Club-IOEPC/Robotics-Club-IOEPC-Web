a47ebd918e2508022414d6fc2a08a78d6b512360
sudipnext <parajulisudip123@outlook.com>
2022-06-15 19:39:31 +0545

    Stable release 7:39PM 2022/06/15

---
+++ per-page/participations/participate.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("china2018").load("./per-page/participations/china2018.html");
+  $("china2019").load("./per-page/participations/china2019.html");
+  $("robocon2016").load("./per-page/participations/robocon2016.html");
+  $("robocon2018").load("./per-page/participations/robocon2018.html");
+  $("setfarm").load("./per-page/participations/setfarm.html");
+  $("vector").load("./per-page/participations/vector.html");
+  $("yantra5").load("./per-page/participations/yantra5.html");
+  $("yantra8").load("./per-page/participations/yantra8.html");
+});
